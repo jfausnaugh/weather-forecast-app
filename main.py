124514447ec5475a974546f3df4f70dad4047818
Juliette Fausnaugh <j.d.land.16@gmail.com>
2024-02-23 13:16:57 -0500

    Plot temperature data in linegraph when user selects Temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 # Add title, text input, slider, select-box and sub-header
 
@@ -9,3 +10,17 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of days to be forecasted.")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+
+if place:
+    # get the temperature/sky data from backend
+    filtered_data = get_data(place, days, option)
+
+    if option == "Temperature":
+        temperatures = [dictionary["main"]["temp"] for dictionary in filtered_data]
+        temperatures = [temp/10 for temp in temperatures]
+        dates = [dictionary["dt_txt"] for dictionary in filtered_data]
+
+        # create a temperature plot
+        figure = px.line(x=dates, y=temperatures,
+                         labels={"x": "Dates", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
