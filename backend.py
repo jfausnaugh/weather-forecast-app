30416777bc19d660950d4814de4c70aaab78eda0
Juliette Fausnaugh <j.d.land.16@gmail.com>
2024-02-23 13:11:32 -0500

    Created backend functioni to get filtered data from the api

--- backend.py
+++ backend.py
@@ -6,3 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 YOUR_API_KEY = os.getenv(key='API_KEY')
 
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={YOUR_API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=1))
